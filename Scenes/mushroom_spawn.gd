649c5b63e14ff317c1b546e6d085b049316ba2a0
tristram003 <tristram003@gmail.com>
2026-08-18 16:10:42 -0400

    Added additional spawners, including a crude customer spawner

--- Scenes/mushroom_spawn.gd
+++ Scenes/mushroom_spawn.gd
@@ -1,15 +1,15 @@
 extends Node3D 
 
+# Spawn count determines how many mushrooms spawn at timers end
 @export var spawn_count: int = 3
 
 @onready var spawn_area: Area3D = $SpawnArea
 @onready var collision_shape: CollisionShape3D = $SpawnArea/CollisionShape3D
 @onready var timer: Timer = $Timer
 
-#@onready var object = $Mushroom1
-@onready var object = $PotionTesting
-
-
+# Mushroom is different objects changed in out scene
+# Might be a better way to architect that
+@onready var object = $Mushroom
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,27 +26,25 @@ func _on_timer_timeout() -> void:
 
 func spawn_object() -> void:
 	var shape = collision_shape.shape
-
 	if shape is SphereShape3D:
+		# Use raduis instead of shape for sphere geometry
 		var radius = shape.radius
-
 		var angle = randf_range(0.0, TAU)
 		var distance = sqrt(randf()) * radius
-
 		var random_x = cos(angle) * distance
 		var random_z = sin(angle) * distance
-
 		var spawn_position = collision_shape.global_position + Vector3(
 			random_x,
 			0.5,
 			random_z
 		)
 
-		#var object_spawn = object_scene.instantiate()
-		#get_tree().current_scene.add_child(object_spawn)
-		#object_spawn.global_position = spawn_position
-#
-#
+		# Duplicates object in scene into random locations
 		var object_spawn = object.duplicate()
 		get_tree().current_scene.add_child(object_spawn)
 		object_spawn.global_position = spawn_position
+
+		## leftover but keep around just in case
+		#var object_spawn = object_scene.instantiate()
+		#get_tree().current_scene.add_child(object_spawn)
+		#object_spawn.global_position = spawn_position
