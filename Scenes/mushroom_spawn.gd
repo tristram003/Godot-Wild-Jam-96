3fd0e1ae6db3af1ef1509a2508d682800da47008
tristram003 <tristram003@gmail.com>
2026-08-18 09:57:03 -0400

    Mushroom Spawn Scipt Added
    Using the previous 'potiontesting' node as the mushroom so we can reuse the interact script.

--- Scenes/mushroom_spawn.gd
+++ Scenes/mushroom_spawn.gd
@@ -1,30 +1,52 @@
-extends Node3D
+extends Node3D 
 
-@export var mushroom_placeholder: MeshInstance3D
+@export var spawn_count: int = 3
+
+@onready var spawn_area: Area3D = $SpawnArea
+@onready var collision_shape: CollisionShape3D = $SpawnArea/CollisionShape3D
+@onready var timer: Timer = $Timer
+
+#@onready var object = $Mushroom1
+@onready var object = $PotionTesting
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+#func _process(delta: float) -> void:
+	#pass
 
 func _on_timer_timeout() -> void:
-	
-	#if musshroom limit not met
-	
-	#random x
-	#random z
-	#mushroom_placeholder.instance
-	
-	
-	#SPAWN MUSHROOMS ON TIMEOUT
-	
-	
-	
-	
-	
+	print("Timer has elasped")
+	for i in spawn_count:
+		spawn_object()
+
+func spawn_object() -> void:
+	var shape = collision_shape.shape
+
+	if shape is SphereShape3D:
+		var radius = shape.radius
+
+		var angle = randf_range(0.0, TAU)
+		var distance = sqrt(randf()) * radius
+
+		var random_x = cos(angle) * distance
+		var random_z = sin(angle) * distance
+
+		var spawn_position = collision_shape.global_position + Vector3(
+			random_x,
+			0.5,
+			random_z
+		)
+
+		#var object_spawn = object_scene.instantiate()
+		#get_tree().current_scene.add_child(object_spawn)
+		#object_spawn.global_position = spawn_position
+#
+#
+		var object_spawn = object.duplicate()
+		get_tree().current_scene.add_child(object_spawn)
+		object_spawn.global_position = spawn_position
