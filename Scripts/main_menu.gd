9931d723c9ab6e076ef41bf8491c6779bb5a7050
tristram003 <tristram003@gmail.com>
2026-08-22 16:56:45 -0400

    Added a Main Menu

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_start_game_pressed() -> void:
+	print("Start Pressed")
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+
+func _on_quit_game_pressed() -> void:
+	print("Quit Pressed")
+	get_tree().quit()
